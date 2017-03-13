1d3dab83ef6157863011302b0b822dde4588420e
Sterling Hanenkamp <sterling@hanenkamp.com>
2017-03-13 04:01:47 +0000

    Adapting the light panel to use light switch

--- light_panel/light_panel.ino
+++ light_panel/light_panel.ino
@@ -6,54 +6,52 @@
 #define ON_AIR_GRID    2
 #define EMERGENCY_GRID 3
 
-#define PIN_GRID            2
-#define PIN_EMERGENCY_PUSH  3
-#define PIN_EMERGENCY_BLINK 4
-#define PIN_ON_AIR_TOGGLE   5
+#define PIN_GRID       2
+#define PIN_ON_AIR     4
+#define PIN_QUIET      5
 
 #define EMERGENCY_DURATION  500
 
 Grid *current;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     pinMode(PIN_GRID, OUTPUT);
-    pinMode(PIN_EMERGENCY_BLINK, OUTPUT);
-    pinMode(PIN_EMERGENCY_PUSH, INPUT);
-    pinMode(PIN_ON_AIR_TOGGLE, INPUT);
+    pinMode(PIN_ON_AIR, INPUT);
+    pinMode(PIN_QUIET, INPUT);
     
     current = new PileGrid();
 }
 
 int mode = PILE_GRID, last_mode = PILE_GRID;
-int emergency_cycles = 0;
+//int emergency_cycles = 0;
 void loop() {
-    int on_air = digitalRead(PIN_ON_AIR_TOGGLE);
-    int emergency = digitalRead(PIN_EMERGENCY_PUSH);
+    int on_air = digitalRead(PIN_ON_AIR);
+    int emergency = digitalRead(PIN_QUIET);
 
     if (on_air == LOW) {
-        emergency_cycles = 0;
+        //emergency_cycles = 0;
         mode = PILE_GRID;
-        digitalWrite(PIN_EMERGENCY_BLINK, LOW);
+        //digitalWrite(PIN_EMERGENCY_BLINK, LOW);
     }
     else {
         if (mode == EMERGENCY_GRID) {
-            if (emergency_cycles-- <= 0) {
-                digitalWrite(PIN_EMERGENCY_BLINK, LOW);
+            //if (emergency_cycles-- <= 0) {
+                //digitalWrite(PIN_EMERGENCY_BLINK, LOW);
                 mode = ON_AIR_GRID;
-            }
+            //}
         }
         else {
-            emergency_cycles = 0;
+            //emergency_cycles = 0;
 
             mode = ON_AIR_GRID;
 
             if (mode == ON_AIR_GRID) {
                 if (emergency == HIGH) {
                     mode = EMERGENCY_GRID;
-                    emergency_cycles = EMERGENCY_DURATION;
-                    digitalWrite(PIN_EMERGENCY_BLINK, HIGH);
+                    //emergency_cycles = EMERGENCY_DURATION;
+                    //digitalWrite(PIN_EMERGENCY_BLINK, HIGH);
                 }
             }
         }
